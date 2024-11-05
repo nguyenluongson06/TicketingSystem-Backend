9d1a5c1031bd45f084bd9b615c3c9ac6202cbc78
Nguyễn Duy Hưng <nguyenluongson06@gmail.com>
2024-11-05 20:49:14 +0700

    add email service

---
+++ src/main/java/com/java2/ticketingsystembackend/controller/EmailController.java
@@ -0,0 +1,15 @@
+﻿package com.java2.ticketingsystembackend.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import com.java2.ticketingsystembackend.service.*;
+
+@RestController
+public class EmailController {
+
+    @Autowired
+    private EmailService emailService;
+}
+
---
+++ src/main/java/com/java2/ticketingsystembackend/service/EmailService.java
@@ -0,0 +1,30 @@
+﻿package com.java2.ticketingsystembackend.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    /**
+     * PRIVATE Send email to given address
+     * can only be called by relevant APIs, never directly exposed as a public API
+     * ex: register, reserveTicket,... will call emailService.sendEmail
+     * @param to address to send email to
+     * @param subject subject of the email
+     * @param body body of the email (can be plain text or HTML)
+     */
+    public void sendEmail(String to, String subject, String body) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("your-email@gmail.com");
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(body);
+        mailSender.send(message);
+    }
+}
